e93175b22f1267bca988007a00e2fff3fa20b520
Madan Patakota <madan.patakota@gmail.com>
2025-10-23 09:48:58 +0530

    tdd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularForms_Oct';
+
+
+  btnClick(registrationForm:NgForm){
+      console.log(registrationForm);
+  }
+
+  
 }
